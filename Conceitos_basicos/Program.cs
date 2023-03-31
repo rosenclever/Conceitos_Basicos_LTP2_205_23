349d57a46c1cb0b1fe0d1654585399c11307ecb1
rosenclever <rosenclever@gmail.com>
2023-03-31 17:33:18 -0300

    lê e imprime as idades

--- Conceitos_basicos/Program.cs
+++ Conceitos_basicos/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//EXERCÍCIOS COM VETORES
+
+/*
+ * crie um vetor para armazenar as idades de 5 pessoas e ao final imprima:
+ * 1) a pessoa mais nova
+ * 2) a pessoa mais velha
+ * 3) a média das idades
+ * 4) todas as idades ímpares
+ * 5) todas as idades pares
+ */
+
+byte[] idades = new byte[5];
+for(int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Informe a {i+1}ª idade:");
+    idades[i] = byte.Parse(Console.ReadLine());
+}
+Console.WriteLine("As idades informadas foram:");
+foreach(byte idade in idades)
+{
+    Console.WriteLine(idade);
+}
